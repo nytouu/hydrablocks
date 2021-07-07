de25a345fa5a265d7948afd0e161cb0a2b85875a
nytouu <capitaine7312@gmail.com>
2021-07-07 23:59:12 +0200

    added scripts

--- config.h
+++ config.h
@@ -1,12 +1,14 @@
 #define CMDLENGTH 100
-#define delimiter " ^c#c0caf5^| "
+#define delimiter "  "
 
 static const Block blocks[] = {
 /*  command             interval    signal */
+    {"bar-record",      0,          9},
     {"bar-song", 		1,          11},
     {"bar-kb",      	0,          8},
 	{"bar-volume",      0,          10},
 	{"bar-bluetooth",   0,          15},
 	{"bar-internet",    10,         4},
 	{"bar-clock",       60,         1},
+    {"bar-power",       0,          26},
 };
