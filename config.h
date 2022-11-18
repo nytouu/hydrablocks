120012c49d175ffbbf3e4eb03379fdcf54e9c470
nytouu <capitaine7312@gmail.com>
2022-11-18 02:55:28 +0100

    config update

--- config.h
+++ config.h
@@ -5,11 +5,13 @@ static const Block blocks[] = {
 /*  command             interval    signal */
     /* {"bar-record",      0,          7}, */
     {"bar-song", 		1,          6},
-    {"bar-kb",      	0,          5},
+    /* {"bar-kb",      	0,          5}, */
 	{"bar-volume",      0,          4},
-	{"bar-notif",       0,          3},
+	{"bar-light",       0,          9},
+	{"bar-battery",     10,         7},
+	/* {"bar-notif",       0,          3}, */
 	/* {"bar-bluetooth",   0,          15}, */
-	{"bar-internet",    10,         2},
+	/* {"bar-internet",    10,         2}, */
 	/* {"bar-calendar",    60,         8}, */
 	{"bar-clock",       60,         1},
     /* {"bar-power",       0,          26}, */
